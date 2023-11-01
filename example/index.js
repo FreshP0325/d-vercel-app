37229e629d3e95f2c56e0a169675cb9061321a35
FreshP0325 <54257356+FreshP0325@users.noreply.github.com>
2023-11-01 12:27:36 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const config = {
   '/': 'https://github.com/FreshP0325/d-vercel-app/blob/master/README.md',
   '/tag': 'Now go, let the legend come back to life!',
   '/hero': 'https://amio.github.io/now-go/ocelot.jpg',
-  'mjj': 'https://www.aidoru.net/assets/bfiles/hostloc.sh',
+  '/mjj': 'https://www.aidoru.net/assets/bfiles/hostloc.sh',
   '*': (req) => {
     if (req.headers.host === 'localhost:3000') {
       return 'Hello from localhost:3000'
